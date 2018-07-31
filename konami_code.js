38e8a581edb5fccedfd755d73aec1668a4811d52
fiolam <fiola.mulliqi@wework.com>
2018-07-31 00:09:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+}
+document.body.addEventListener("keydown", init)
+  let index = 0
+function init (){
+  debugger;
 
+  let key= event.which;
+if (key===code[index]){
+  index++
+if (index === code.length){
+  Alert=("Konami");
+}
+}
+else {
+  index = 0;
+}
 }
