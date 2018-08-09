cec7af4f4b7c07f1f05a6e21092223d6c4e7a94b
Lisa Mao <liisamao@gmail.com>
2018-08-09 00:12:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+document.body.addEventListener("keydown", playKonami);
+ // console.log(init.key);
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function playKonami(event) {
+  debugger;
+  let key = event.which;
+  if (key === code[index]){
+    index++
+    if (index === code.length){
+      alert("konami");
+      index=0;
+    }
+  } else{
+    alert('Wrong Code')
+    index=0;
+  }
 }
+
